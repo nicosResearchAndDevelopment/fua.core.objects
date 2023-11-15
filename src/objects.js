d0db810c740e72e26a38ce65be98f8aa2f788a67
Simon Petrac <pfoerdie@gmail.com>
2023-11-15 16:21:10 +0100

    added array methods

--- src/objects.js
+++ src/objects.js
@@ -250,5 +250,58 @@ objects.hide.props = function (target, ...keys) {
     return target;
 };
 
+/**
+ * Converts any value to an array.
+ * If the value is undefined or null, the result will be an empty array.
+ * If the value is a native datatype, the result will be an array with the value as only entry.
+ * If the value is an array itself, the result will be as is and the same instance as the value.
+ * If the value is an iterable object or an iterator instance, the result will be an array with the collected entries.
+ * If the value is a function or an object, the result will be an array with the value as only entry.
+ * In any other case, which there should be none, the array is empty for a falsy value and has one entry for a truthy value.
+ * @param {undefined|boolean|number|bigint|string|symbol|null|Array|TypedArray|Iterable|Iterator|Object|function|any} value
+ * @returns {Array}
+ */
+objects.array = function (value) {
+    switch (typeof value) {
+        case 'undefined':
+            return [];
+        case 'boolean':
+        case 'number':
+        case 'bigint':
+        case 'string':
+        case 'symbol':
+            return [value];
+        case 'object':
+            if (!value) return [];
+            if (util.isArray(value)) return value;
+            if (util.isFunction(value[Symbol.iterator])) return Array.from(value);
+            if (util.isFunction(value.next)) {
+                const result = [];
+                let current  = value.next();
+                while (current?.done === false) {
+                    result.push(current.value);
+                    current = value.next();
+                }
+                if (current?.done === true) return result;
+            }
+            return [value];
+        case 'function':
+            return [value];
+        default:
+            if (!value) return [];
+            return [value];
+    }
+};
+
+/**
+ * Like toArray, this method converts any value to an array, but then freezes it afterwards.
+ * @param {undefined|boolean|number|bigint|string|symbol|null|Array|TypedArray|Iterable|Iterator|Object|function|any} value
+ * @returns {Readonly<Array>}
+ */
+objects.array.freeze = function (value) {
+    const result = objects.array(value);
+    return Object.freeze(value === result ? [...result] : result);
+};
+
 objects.freeze.recursive(objects);
 module.exports = objects;
